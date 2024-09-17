b564e69fa246c60bd6ca7baf37930c65d9586832
Caio Mognatti <caiocsm97@gmail.com>
2024-09-17 11:54:21 -0300

    fix: changes the globab styles to a ts file

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,15 +1,15 @@
 import styled from "styled-components";
-import { styled as styledMui } from "@mui/material/";
 import Autocomplete from "@mui/material/Autocomplete/Autocomplete";
-import { Button } from "@mui/material";
+import { styled as styledMui, Button } from "@mui/material";
 
 export const pallete = {
-  green: "#739245",
-  lightGreen: "#95a887",
-  lighterGreen: "#c3ccbf",
-  lime: "#8daf36",
-  dark: "#435b37",
   white: "#fefefe",
+  offWhite: "#e6ebdf",
+  lighterGreen: "#c3ccbf",
+  lightGreen: "#95a887",
+  green: "#739245",
+  dark: "#435b37",
+  black: "#000",
 };
 
 export const breakPoints = {
@@ -26,23 +26,20 @@ export const ListContainer = styled.div`
 export const List = styled.ul`
   display: flex;
   flex-direction: row;
-  align-content: center;
   flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: center;
   padding-top: 8px;
   padding-left: 0;
-  width: 100%;
   gap: 3vh 3vw;
   @media (max-width: ${breakPoints.mobile}) {
+    margin-left: -4vw;
     flex-direction: column;
   }
 `;
 export const Item = styled.li`
-  width: 30%;
+  width: 300px;
   list-style: none;
-  @media (max-width: ${breakPoints.mobile}) {
-    padding-left: 15vw;
-    width: 100%;
-  }
 `;
 export const NotLoggedin = styled.p`
   text-align: center;
@@ -78,34 +75,29 @@ export const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
   background-color: rgba(69, 80, 61, 0.4);
-  border-radius: 15px;
+  border-radius: 16px;
   padding: 8px;
   width: 100%;
   box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.4);
   @media (max-width: ${breakPoints.mobile}) {
-    width: 80%;
+    width: 100%;
   }
 `;
 export const CardInfoList = styled.ul`
   display: flex;
   flex-direction: column;
-  padding: 8px;
   width: 100%;
+  padding: 0px;
 `;
 export const CardTitleDiv = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 8px;
 `;
 export const Info = styled.li`
-  padding: 18px;
-  font-size: 15px;
+  padding: 16px 4px;
+  font-size: 14px;
   border-bottom: 1px solid rgba(195, 204, 191, 0.5);
-  width: 300px;
-  @media (max-width: ${breakPoints.mobile}) {
-    width: 80%;
-  }
 `;
 export const AutoComplete = styledMui(Autocomplete)`
 width:64%;
@@ -130,13 +122,10 @@ export const CreateButtonContainer = styled.div`
 export const Section = styled.section<{ sidebar?: boolean }>`
   display: flex;
   flex-direction: column;
-  margin-left: ${(props) => (props.sidebar ? "200px" : "60px")};
-  margin-top: 5vh;
-  padding: 5%;
   transition: all 300ms ease;
-  @media (max-width: 800px) {
-    margin-left: 55px;
-    padding: 0;
+  margin: 32px 80px;
+  @media (max-width: ${breakPoints.tablet}) {
+    margin: 4vh 4vw 10vh 4vw;
   }
 `;
 export const SessionTitle = styled.h1`
