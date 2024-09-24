e540995a3f78a0facfcb6a090cc9704c4fbed3b1
Caio Mognatti <caiocsm97@gmail.com>
2024-09-24 16:49:54 -0300

    fix: fixes a typo

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 import Sidebar from "./components/Sidebar";
 import Calendar from "./pages/Calendario";
 import Clients from "./pages/Cliente";
-import NotFount from "./pages/NotFound";
+import NotFound from "./pages/NotFound";
 import Financeiro from "./pages/Financeiro";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./styles/theme";
@@ -38,7 +38,7 @@ export default function AppRouter() {
             <Route path="/clientes" element={<Clients />} />
             <Route path="/financeiro" element={<Financeiro />} />
             <Route path="/procedimentos" element={<Procedimentos />} />
-            <Route path="*" element={<NotFount />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
           </Routes>
         </SidebarStatusProvider>
       </Router>
