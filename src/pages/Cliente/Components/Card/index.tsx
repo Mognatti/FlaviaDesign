ce083359f4e4cbebe0e7c5d9c84f0a5b4d1eee58
Caio Mognatti <caiocsm97@gmail.com>
2024-09-24 17:02:44 -0300

    feat: removes a typo

--- src/pages/Cliente/Components/Card/index.tsx
+++ src/pages/Cliente/Components/Card/index.tsx
@@ -29,7 +29,7 @@ export default function Card({ costumer }: any) {
         </CardTitleDiv>
         <Info>Telefone: {costumer.cel_number}</Info>
         <Info>
-          Atendimento mais recente:{" "}
+          Atendimento mais recente:
           {costumer.last_visit !== null ? dayjs(costumer.last_visit).format("DD/MM/YY") : "Não registrado"}
         </Info>
         <Info>Último serviço: {costumer.last_service}</Info>
