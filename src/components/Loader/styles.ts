1bd275a70dd90142f70cf712a2e4f89108fdad91
Caio Mognatti <caiocsm97@gmail.com>
2024-09-25 22:07:36 -0300

    fix: adjust the loading page to prevent scroll

--- src/components/Loader/styles.ts
+++ src/components/Loader/styles.ts
@@ -5,6 +5,6 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  height: 95vh;
 `;
 export const LoadingAnimation = styled(PuffLoader)``;
